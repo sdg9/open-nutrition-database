7c677cd3e361210195715d7b0d070e987d3949d0
Ken Mazaika <kenmazaika@gmail.com>
2013-03-29 23:20:28 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenNutritionDatabase::Application.config.secret_key_base = '8a7cf2ea0706f74d29aa2cfc0834bcf972958f866d48fb87a9e629c1adfb2af151e75cb7952837ad3685e1e54f211dbd903555e39bb17899da46f618f4c3ac55'
